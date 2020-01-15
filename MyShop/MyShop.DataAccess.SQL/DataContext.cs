ad642ad9c3fec362597d62c25412fb86e89695cc
franksammi <40390931+franksammi@users.noreply.github.com>
2020-01-15 16:16:03 +0300

    Added Basket Model

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -17,5 +17,7 @@ public class DataContext :DbContext
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
